cf0e19765abc86fc03d6fc87fc1e6b53092a85a5
Miodrag Orbovic <43953560+miodragorbovic1988@users.noreply.github.com>
2018-11-06 18:03:07 +0100

    izmena na movie i movie festival folderima

--- movie/js/form.js
+++ movie/js/form.js
@@ -1,16 +1,54 @@
-function getMovieFunction() {
-    var title = document.querySelector(".firstInput").value;
-    var length = document.querySelector(".secondInput").value;
-    var selectedGenre = document.querySelector("#movieGenre").value;
 
-    var genre = new Genre(selectedGenre);
-    var movie = new Movie(title, length, genre);
+var $button = document.querySelector('.firstButton');
 
-    //console.log(movie.getData());
 
-    document.querySelector("#allMovieData").innerHTML = movie.getData();
+$button.addEventListener('click', getMovieFunction);
+
+function getMovieFunction(event) {
+    var title = document.querySelector(".firstInput");
+    var length = document.querySelector(".secondInput");
+    var selectedGenre = document.querySelector("#movieGenre");
+
+
+    var newTitle = title.value;
+    var newLength = length.value;
+    var newSelectedGenre = selectedGenre.value;
+
+    var genre = new Genre(newSelectedGenre);
+    var movie = new Movie(newTitle, newLength, genre);
+
+
+    document.querySelector("#allMovieData").innerHTML += movie.getData() + '<br>';
+
+    title.value = '';
+    length.value = '';
+    selectedGenre.value = '-';
+
+    var movieSelect = document.querySelector('#movieSelect');
+    var createNewOption = document.createElement('option');
+    createNewOption.textContent = movie.getData()
+
+    movieSelect.appendChild(createNewOption);
+
+
 }
 
+// var $program = document.querySelector('.createProgram');
+
+// $program.addEventListener('click', createProgramFunction);
+// function createProgramFunction(event) {
+//     var newDate = date.value;
+//     document.querySelector('#createdProgram').innerHTML += date.value;
+// }
 
 
-// ******************************** 
+
+var $choseDate = document.querySelector('.createProgram');
+
+$choseDate.addEventListener('click', getProgramData);
+
+function getProgramData(event) {
+    var getFullProgram = document.querySelector('.choseDate').value;
+
+    document.querySelector("#createdProgram").innerHTML += getFullProgram + movie.getData() + '<br>';
+}
